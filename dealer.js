124744d87257afe2cbdd25d1d19dd09878d2fcde
Dam <adamhafiz797@gmail.com>
2025-06-14 14:43:56 +0800

    perbaikan format dan penambahan z-index pada elemen draggable

--- dealer.js
+++ dealer.js
@@ -1,4 +1,4 @@
-var brandlist = new Array("Porsche","Volkswagen","Audi","BMW");
+var brandlist = new Array("Porsche", "Volkswagen", "Audi", "BMW");
 
 var gameStats = {
 	clientServed: 0,
@@ -14,52 +14,58 @@ var carPrices = {
 }
 
 
-function newClient(){
-	var preference = Math.floor((Math.random()*4));
-	var time = Math.floor((Math.random()*10000)+1);
-	var client = Math.floor((Math.random()*10)+1);
-	$("#clients_queue").append('<div class="client client_'+client+'"><span class="preference">Client for '+brandlist[preference]+'</span></div>');
-	setTimeout(function(){newClient();},time);
-	$(".client").draggable();
-	
+function newClient() {
+	var preference = Math.floor((Math.random() * 4));
+	var time = Math.floor((Math.random() * 10000) + 1);
+	var client = Math.floor((Math.random() * 10) + 1);
+	$("#clients_queue").append('<div class="client client_' + client + '"><span class="preference">Client for ' + brandlist[preference] + '</span></div>');
+	setTimeout(function () { newClient(); }, time);
+	$(".client").draggable({
+		containment: "#salon",
+		revert: "invalid",
+	});
+
 }
-$("document").ready(function(e) {
+$("document").ready(function (e) {
 	newClient();
 
-	    $( ".place" ).droppable({
-      drop: function( event, ui ) {
-        alert('dropped')
-      }
-    });
-	$( "#cashier" ).droppable({
-      drop: function( event, ui ) {
-		var isPurchased = confirm("Would you like to purchase this car?");
-		if(isPurchased == true){
-			gameStats.clientServed++;
-			gameStats.totalAmount = gameStats.totalAmount + carPrices.Porsche;
-			gameStats.carssold++;
-		}else{
+	$(".place").droppable({
+		drop: function (event, ui) {
+			var client = (ui.draggable);
+			var carPlace = $(this);
+			carPlace.append(client);
+			client.css({ 'position': 'absolute', 'top': '1px', 'left': '1px' });
+		}
+	});
+	$("#cashier").droppable({
+		drop: function (event, ui) {
+			var isPurchased = confirm("Would you like to purchase this car?");
+			if (isPurchased == true) {
+				gameStats.clientServed++;
+				gameStats.totalAmount = gameStats.totalAmount + carPrices.Porsche;
+				gameStats.carssold++;
+			} else {
 				gameStats.clientServed++;
 			}
-       gameStats.totalAmount = gameStats.totalAmount + carPrices.BMW;
-		gameStats.clientServed++;
-		gameStats.carssold++;
-       updateStats();
-		ui.draggable.remove();
-		updateStats();
-      }
-    });
-	$( "#exit" ).droppable({
-      drop: function( event, ui ) {
-		gameStats.clientServed++;
-		updateStats();
-		ui.draggable.remove();
-      }
-    });
+			gameStats.totalAmount = gameStats.totalAmount + carPrices.BMW;
+			gameStats.clientServed++;
+			gameStats.carssold++;
+			updateStats();
+			ui.draggable.remove();
+			updateStats();
+		}
+	});
+	$("#exit").droppable({
+		drop: function (event, ui) {
+			gameStats.clientServed++;
+			updateStats();
+			ui.draggable.remove();
+		}
+	});
 });
 
-function updateStats(){
-		$("#clients_served" ).text(gameStats.clientServed + "clients")
-		$("#cars_sold" ).text(gameStats.carssold + "cars")
-		$("#amount" ).text(gameStats.totalAmount + " €"	)
+function updateStats() {
+	$("#clients_served").text(gameStats.clientServed + "clients")
+	$("#cars_sold").text(gameStats.carssold + "cars")
+	$("#amount").text(gameStats.totalAmount + " €")
 }
